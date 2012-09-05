7ec952a742d0388ef8ded2304a5b8ced5201e1c8
ChunAllen <achun@appsource.biz>
2012-09-05 13:52:50 +0800

    updated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120905043649) do
+ActiveRecord::Schema.define(:version => 20120905053125) do
 
   create_table "homes", :force => true do |t|
     t.datetime "created_at", :null => false
