bd38a18acef6706b8cdf9b72df8832d1af7eed1d
ChunAllen <achun@appsource.biz>
2012-09-05 16:41:38 +0800

    fixed setup page with css

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rms::Application.routes.draw do
   devise_for :users
 
+
+  resources :companies
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -50,7 +52,7 @@ Rms::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'homes#index'
+   root :to => 'company#index'
 
   # See how all your routes lay out with "rake routes"
 
