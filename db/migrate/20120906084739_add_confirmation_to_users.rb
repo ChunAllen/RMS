3b1c42e9c25d9480f406a6a6b6bf7eef0b4da07d
ChunAllen <achun@appsource.biz>
2012-09-06 17:11:32 +0800

    email verification

--- db/migrate/20120906084739_add_confirmation_to_users.rb
+++ db/migrate/20120906084739_add_confirmation_to_users.rb
@@ -1,6 +1,6 @@
 class AddConfirmationToUsers < ActiveRecord::Migration
   def change
-  	radd_column :users, :confimed_at, :datetime
+  	add_column :users, :confimed_at, :datetime
   	add_column :users, :confirmation_token, :string
  	add_column :users, :confirmation_sent_at, :datetime
   end
