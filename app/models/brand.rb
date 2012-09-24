af0f3b1c1b6d808fa38c8c62788f6bd577269697
markaventura <maventura@appsource.biz>
2012-09-24 16:42:12 +0800

    create brand and brancges

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -2,6 +2,10 @@ class Brand < ActiveRecord::Base
   attr_accessible :name, :client_id, :description,:photo_file_name,:photo_content_type,
   :photo_file_size,:photo_updated_at,:photo
   
+
+  has_attached_file :photo,
+  :default_url => '/assets/icons/company.png'
+
   belongs_to :client
   has_many :branches
   
--- app/models/client.rb
+++ app/models/client.rb
@@ -2,7 +2,7 @@ class Client < ActiveRecord::Base
   attr_accessible  :name,:description, :status,:photo_file_name, :photo_content_type, :photo_file_size,:photo_updated_at, :photo, :user_id
 
   has_attached_file :photo,
-  :default_url => '/assets/images/company.png'
+  :default_url => '/assets/icons/company.png'
   belongs_to :user
   has_many :brands
 end
